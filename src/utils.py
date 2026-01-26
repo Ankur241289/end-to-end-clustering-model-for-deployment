cec1d2a4e1aff748f41a38d6609c33fd13615177
Ankur241289 <ankursingh@outlook.com>
2026-01-26 11:51:49 +0530

    Hyperparameter training added

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,11 @@
 import os
 import sys
+from catboost import CatBoostRegressor
 import pandas as pd
 import numpy as np
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 from src.exception import CustomException
@@ -21,23 +23,39 @@ def save_object(file_path,obj):
     
 
 
-def evaluate_model( X_train, y_train, X_test, y_test, models):
-    try:
-        report={}
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
+def evaluate_model(X_train, y_train, X_test, y_test, models:dict, param:dict):
+    report = {}
 
-            model.fit(X_train, y_train)
+    for name, model in models.items():
+        try:
+            # Special handling for CatBoost (not fully sklearn compatible)
+            if isinstance(model, CatBoostRegressor):
+                # Use CatBoost's own grid_search
+                grid_result = model.grid_search(param.get(name, {}), 
+                                                X=X_train, 
+                                                y=y_train, 
+                                                cv=3, 
+                                                verbose=False)
+                # After grid_search, model is already fitted
+                y_test_pred = model.predict(X_test)
+                test_model_score = r2_score(y_test, y_test_pred)
+                report[name] = test_model_score
 
-            y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
+            else:
+                # Standard sklearn models
+                gs = GridSearchCV(model, param.get(name, {}), cv=3, n_jobs=-1, verbose=0)
+                gs.fit(X_train, y_train)
 
-            train_model_score = r2_score(y_train, y_train_pred)
-            test_model_score = r2_score(y_test, y_test_pred)
+                best_model = gs.best_estimator_
+                y_test_pred = best_model.predict(X_test)
+                test_model_score = r2_score(y_test, y_test_pred)
+                report[name] = test_model_score
 
-            report[list(models.keys())[i]] = test_model_score
-        
-        return report
-    
-    except Exception as e:
-        raise CustomException(e, sys)
+                # Replace unfitted model with fitted best_model
+                models[name] = best_model
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+    return report
+
